98df591fde8735be3b5a605ff36438092433673a
echosoar <82163514@qq.com>
2021-08-11 13:22:14 +0800

    fix: deploy env

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const { execSync } = require('child_process');
   
 
   console.log('Step 3: Exec Deploy');
-  const res = execSync(cmd);
+  const res = execSync(`SERVERLESS_DEPLOY_ID=${process.env.INPUT_ID} SERVERLESS_DEPLOY_AK=${process.env.INPUT_AK} SERVERLESS_DEPLOY_SECRET=${process.env.INPUT_SECRET} SERVERLESS_DEPLOY_ENDPOINT=${process.env.INPUT_ENDPOINT} SERVERLESS_DEPLOY_TIMEOUT=${process.env.INPUT_TIMEOUT || '1000'} ${cmd}`);
   console.log(res.toString());
   
   // await exec(`${cmd}`, args, {
