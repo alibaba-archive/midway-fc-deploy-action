d9d0daf76f1a4b88acdb3fa6dd18ac739adf0370
echosoar <82163514@qq.com>
2021-08-11 12:16:57 +0800

    fix: quote reverse

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { exec } = require('@actions/exec');
   await exec('npm i @midwayjs/cli');
 
   let pkgJson = {};
-  let cmd = `node -p 'require(\'${join(process.cwd(), 'node_modules/@midwayjs/cli/bin/cli.js')}\').cli({ _: [\'deploy\'], yes: true, V: true })'`;
+  let cmd = `node -p 'require(\\\'${join(process.cwd(), 'node_modules/@midwayjs/cli/bin/cli.js')}\\\').cli({ _: [\\\'deploy\\\'], yes: true, V: true })'`;
   let args = [];
   try {
     pkgJson = JSON.parse(readFileSync(join(cwd, 'package.json')).toString());
