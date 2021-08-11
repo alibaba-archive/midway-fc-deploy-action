7dfd2171fa7358698e441727da2d0d394969ec05
echosoar <82163514@qq.com>
2021-08-11 13:27:53 +0800

    feat: complete action

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const { readFileSync } = require('fs');
 const { join } = require('path');
-const { exec } = require('@actions/exec');
 const { execSync } = require('child_process');
 
 ;(async () => {
   const cwd = process.cwd();
   console.log('Step 1: Install Deps');
-  await exec('npm i');
+  await execSync('npm i');
 
   console.log('Step 2: Install Deploy Tools');
-  await exec('npm i @midwayjs/cli');
+  await execSync('npm i @midwayjs/cli');
 
   let pkgJson = {};
   let cmd = `npx node deploy --yes -V`;
@@ -23,19 +22,7 @@ const { execSync } = require('child_process');
     args = [];
   }
 
-  
-
   console.log('Step 3: Exec Deploy');
   const res = execSync(`SERVERLESS_DEPLOY_ID=${process.env.INPUT_ID} SERVERLESS_DEPLOY_AK=${process.env.INPUT_AK} SERVERLESS_DEPLOY_SECRET=${process.env.INPUT_SECRET} SERVERLESS_DEPLOY_ENDPOINT=${process.env.INPUT_ENDPOINT} SERVERLESS_DEPLOY_TIMEOUT=${process.env.INPUT_TIMEOUT || '1000'} ${cmd}`);
   console.log(res.toString());
-  
-  // await exec(`${cmd}`, args, {
-  //   env: {
-  //     SERVERLESS_DEPLOY_ID: process.env.INPUT_ID,
-  //     SERVERLESS_DEPLOY_AK: process.env.INPUT_AK,
-  //     SERVERLESS_DEPLOY_SECRET: process.env.INPUT_SECRET,
-  //     SERVERLESS_DEPLOY_ENDPOINT: process.env.INPUT_ENDPOINT,
-  //     SERVERLESS_DEPLOY_TIMEOUT: process.env.INPUT_TIMEOUT || '1000',
-  //   }
-  // });
 })();
