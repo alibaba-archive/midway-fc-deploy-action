bb3f98c28c1828a775d585b74d8be294d81aab8c
echosoar <82163514@qq.com>
2021-08-11 11:39:31 +0800

    fix: using github actions

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const { execSync } = require('child_process');
 const { readFileSync } = require('fs');
 const { join } = require('path');
+const { exec } = require('@actions/exec');
 
 ;(async () => {
   console.log('ENV', JSON.stringify(process.env, null, 2));
   const cwd = join(process.env.GITHUB_WORKSPACE, '../');
   console.log('Step 1: Install Deps');
-  const lsRes = execSync(`cd ${cwd}; ls -al`);
-  console.log(lsRes.toString());
-  execSync(`cd ${cwd}; npm i`);
+  await exec('ls -al');
+  await exec('npm i');
 
   console.log('Step 2: Install Deploy Tools');
-  execSync(`cd ${cwd}; npm i @midwayjs/cli`);
+  await exec('npm i @midwayjs/cli');
 
   let pkgJson = {};
-  let cmd = `npx mw deploy --yes -V`;
+  let cmd = `npx mw deploy`;
+  let args = ['--yes', '-V'];
   try {
     pkgJson = JSON.parse(readFileSync(join(cwd, 'package.json')).toString());
   } catch (e) {}
 
   if (pkgJson.scripts && pkgJson.scripts.deploy) {
     cmd = `npm run deploy`;
+    args = [];
   }
 
   console.log('Step 3: Exec Deploy');
-  const deployRes = execSync(`cd ${cwd}; SERVERLESS_DEPLOY_ID=${process.env.INPUT_ID} SERVERLESS_DEPLOY_AK=${process.env.INPUT_AK} SERVERLESS_DEPLOY_SECRET=${process.env.INPUT_SECRET} SERVERLESS_DEPLOY_ENDPOINT=${process.env.INPUT_ENDPOINT || ''} SERVERLESS_DEPLOY_TIMEOUT=${process.env.INPUT_TIMEOUT || '1000'} ${cmd}`);
-  console.log(deployRes.toString());
+  await exec(`${cmd}`, args, {
+    env: {
+      SERVERLESS_DEPLOY_ID: process.env.INPUT_ID,
+      SERVERLESS_DEPLOY_AK: process.env.INPUT_AK,
+      SERVERLESS_DEPLOY_SECRET: process.env.INPUT_SECRET,
+      SERVERLESS_DEPLOY_ENDPOINT: process.env.INPUT_ENDPOINT,
+      SERVERLESS_DEPLOY_TIMEOUT: process.env.INPUT_TIMEOUT || '1000',
+    }
+  });
 })();
